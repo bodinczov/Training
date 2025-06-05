ac22298c5be22b4acc16679e060b98ba18514c62
Борис <boris@MacBook-Pro-Boris.local>
2025-06-05 12:15:15 +0300

    init

---
+++ src/main/java/org/training/Main.java
@@ -0,0 +1,9 @@
+package org.training;
+
+import org.training.collections.MyArrayList;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+    }
+}
