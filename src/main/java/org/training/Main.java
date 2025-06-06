9df050ba580d2aaf0cb9fe2fe7923eeb2db8bf8b
bodinczov <boris.od.251@mail.ru>
2025-06-06 17:04:36 +0300

    Update Main.java

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -5,5 +5,6 @@ import org.training.collections.MyArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+                System.out.println("Hello, World!");
     }
 }
