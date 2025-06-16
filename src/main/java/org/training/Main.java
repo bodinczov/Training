e9cb189ef0dd904b75ef6b39b108f2a3cbaf2b9c
Борис <boris@MacBook-Pro-Boris.local>
2025-06-16 13:26:41 +0300

    MyArrayList finished

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -1,9 +1,33 @@
 package org.training;
 
 import org.training.collections.MyArrayList;
+import org.training.collections.MyHashMap.MyHashMap;
+import org.training.collections.MyLinkedList.MyLinkedList;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+//        System.out.println("----- MyArrayList -----");
+//        MyArrayList<Integer> a = new MyArrayList<Integer>();
+//        for(int i = 0; i < 11; i++){
+//            a.add(i);
+//        }
+//        System.out.println(a);
+//        a.remove(10);
+//        System.out.println(a);
+//        a.add(10, 10);
+//        System.out.println(a);
+//
+//        System.out.println("----- MyLinkedList -----");
+//        MyLinkedList<Integer> linkedList = new MyLinkedList<>();
+//        linkedList.addFirst(10);
+//        System.out.println(linkedList);
+//        linkedList.add(1, 25);
+//        System.out.println(linkedList);
+//
+//        MyHashMap<Integer, String> b = new MyHashMap<>();
+//        String l = "AAA";
+//        for (int i = 0; i < 1000; i++){
+//            b.put(i, l);
+//        }
     }
 }
