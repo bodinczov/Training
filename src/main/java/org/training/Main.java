eac2aba08dc8d9dff45d6a6edf11f2cdb094bd33
bodinczov <boris.od.251@mail.ru>
2025-06-16 13:12:58 +0300

    Revert "Update Main.java"

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -5,6 +5,5 @@ import org.training.collections.MyArrayList;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
-                System.out.println("Hello, World!");
     }
 }
