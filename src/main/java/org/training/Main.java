aeccd34eb04ef336a137066d9cc5bc80a3c4f7a3
bodinczov <boris.od.251@mail.ru>
2025-06-24 12:44:59 +0300

    HashMap finished (#5)
    * MyLinkedList finished
    
    * HashMap finished
    
    * prepared to pull-request
    
    * MyHashMap improved
    
    * fix Main
    
    ---------
    
    Co-authored-by: Борис <boris@MacBook-Pro-Boris.local>
    Co-authored-by: fdobrynin <fdobrynin@sigma-it.ru>

--- src/main/java/org/training/Main.java
+++ src/main/java/org/training/Main.java
@@ -1,6 +1,7 @@
 package org.training;
 
 import org.training.collections.MyArrayList;
+import org.training.collections.MyHashMap.MyHashMap;
 import org.training.collections.MyLinkedList.MyLinkedList;
 
 public class Main {
@@ -40,5 +41,27 @@ public class Main {
         System.out.println("First: " + myLinkedListTest.getFirst());
         System.out.println("Last: " + myLinkedListTest.getLast());
         System.out.println("Size: " + myLinkedListTest.getSize());
+
+        System.out.println("----- MyHashMap -----");
+        MyHashMap<String, Integer> myHashMap = new MyHashMap<>();
+        System.out.println("isEmpty: " + myHashMap.isEmpty());
+        System.out.println("size: " + myHashMap.size());
+        myHashMap.put("One", 1);
+        myHashMap.put("Two", 2);
+        myHashMap.put("Three", 3);
+        myHashMap.put("Four", 4);
+        System.out.println("after put: " + myHashMap);
+        System.out.println("size: " + myHashMap.size());
+        System.out.println("get by key: " + myHashMap.get("Two"));
+        myHashMap.put("Two", 22);
+        System.out.println("update value: " + myHashMap);
+        System.out.println("contains 'Three': " + myHashMap.containsKey("Three"));
+        System.out.println("contains 'Ten': " + myHashMap.containsKey("Ten"));
+        myHashMap.remove("One");
+        System.out.println("after remove: " + myHashMap);
+        System.out.println("size: " + myHashMap.size());
+        myHashMap.put(null, 999);
+        System.out.println("null key put: " + myHashMap);
+        System.out.println("get null key: " + myHashMap.get(null));
     }
 }
